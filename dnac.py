0670b8a532a14032e9e8d3de27db9278b613c432
Chetan Batra (cbatra) <cbatra@cisco.com>
2020-06-02 15:15:41 +0530

    Assignment2 commit

--- dnac.py
+++ dnac.py
@@ -18,7 +18,7 @@ def getAuth(user,pswd,url):
 
 def getDevices(url,token):
     payload = {}
-    headers = {'x-auth-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI1ZWNlNTc5ODc1MTYxMjAwY2M1NzA2M2QiLCJhdXRoU291cmNlIjoiaW50ZXJuYWwiLCJ0ZW5hbnROYW1lIjoiVE5UMCIsInJvbGVzIjpbIjVlNWE0MzI1NzUxNjEyMDBjYzRhYzk1ZSJdLCJ0ZW5hbnRJZCI6IjVlNWE0MzI1NzUxNjEyMDBjYzRhYzk1YyIsImV4cCI6MTU5MTA4OTc0MywiaWF0IjoxNTkxMDg2MTQzLCJqdGkiOiJiOTM4Zjc1ZS04Nzc2LTQwYTUtYTkxYy04MDU3ODY5MWU3YzUiLCJ1c2VybmFtZSI6ImRuYWNkZXYifQ.eVFAFH8ySVd1sUlxNDAwu2CtrPwauWa1xTkv5zz6GWmIrnY1PvtIZKEm1unuFnfBEfT4v_OKnZc2jlt1JHgd1j5yzf0HvlafP3QDNo5qn5Ol2yEtj9tqXRmBel5aNfyhH1aE2S5sTsUREb-hM6MelwUdXmWHM0xwZcs19Wd84qAOsqxID-BEh5QBhgyvMgoYWJv8Y0-PTMq8KJMfqfgIHga-Q4RMhv0VZDILAG-G7-bHUSKIfFfxbfIv_RWTyvUvwXHvwDrERb49AA4HqDcdWus8yHsMmwhn0HGmoR28XWMjW02jiBgC2jY-jotk_AJHMlictc9uoe-aottIOwxQRA'}
+    headers = {'x-auth-token': token }
 
     response = requests.request("GET", url, headers=headers, data = payload)
     obj=json.loads(response.text)["response"]
