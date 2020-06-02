03e9996418268fa31fa0da6cfec6ed6e4a36d9cd
Chetan Batra (cbatra) <cbatra@cisco.com>
2020-06-02 15:32:44 +0530

    Master revert

--- parser2.py
+++
@@ -1,9 +0,0 @@
-
-def read(dat):
-    for el in dat:
-        print('---------------------------------')
-        print('id: ',el['id'])
-        print('type: ',el['type'])
-        print('family: ',el['family'])
-        print('softwareType: ',el['softwareType'])
-        print('managementIpAddress: ',el['managementIpAddress'])
