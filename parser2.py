ac06f54b163a54cd1318e53ba9c6f50a9a911713
Chetan Batra (cbatra) <cbatra@cisco.com>
2020-06-02 14:18:25 +0530

    Assignment2 tasks

---
+++ parser2.py
@@ -0,0 +1,9 @@
+
+def read(dat):
+    for el in dat:
+        print('---------------------------------')
+        print('id: ',el['id'])
+        print('type: ',el['type'])
+        print('family: ',el['family'])
+        print('softwareType: ',el['softwareType'])
+        print('managementIpAddress: ',el['managementIpAddress'])
